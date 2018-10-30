00be4db037c63de0f74e50ea5f9ea4fd2becbb4a
Tomas Ledesma <l_tom19@hotmail.com>
2018-10-30 20:39:06 -0300

    added current position

--- public/jsscript/index.js
+++ public/jsscript/index.js
@@ -1,7 +1,39 @@
-function initMap(){
-    var options = {
-        zoom : 12,
-        center : {lat: -31.3112115, lng: -64.283903215}    
+function initMap() {
+    var currentPositionMarker;
+    navigator.geolocation.getCurrentPosition(function(e) {
+      var options = {
+        zoom: 15,
+        center: { lat: e.coords.latitude, lng: e.coords.longitude }
+      };
+      var map = new google.maps.Map(document.getElementById("map"), options);
+  
+      currentPositionMarker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(
+          e.coords.latitude,
+          e.coords.longitude
+        ),
+        title: "Current Position"
+      });
+      map.panTo(
+        new google.maps.LatLng(e.coords.latitude, e.coords.longitude)
+      );
+  
+      watchCurrentPosition()
+    });
+  
+    var watchCurrentPosition = function() {
+      var positionTimer = navigator.geolocation.watchPosition(function(position) {
+        setMarkerPosition(currentPositionMarker, position);
+      });
+    };
+  
+    function setMarkerPosition(marker, position) {
+      marker.setPosition(
+        new google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        )
+      );
     }
-    var map = new google.maps.Map(document.getElementById('map'), options );
-}
+  }
